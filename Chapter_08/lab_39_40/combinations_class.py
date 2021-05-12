27d962801dd40a1f47ce2733147959ecca09c801
pangeon <kamil.cecherz@gmail.com>
2021-05-12 13:12:50 +0200

    iteration with __getitem__ - solution 1

--- Chapter_08/lab_39_40/combinations_class.py
+++ Chapter_08/lab_39_40/combinations_class.py
@@ -4,32 +4,25 @@ class Combinations:
         self.products = products
         self.promototions = promototions
         self.customers = customers
-
-        self.current_product = 0
-        self.current_promotion = 0
-        self.current_customer = 0
-
-    def __next__(self):
-        if self.current_customer >= len(self.customers):
-            self.current_customer = 0
-            self.current_promotion += 1
-
-        if self.current_promotion >= len(self.promototions):
-            self.current_promotion = 0
-            self.current_product += 1
-
-        if self.current_product >= len(self.products):
-            self.current_product = 0
-            raise StopIteration("Stopped iteration...")
-        
-        item_to_return = "{} - {} - {}".format(
-            self.products[self.current_product],
-            self.promototions[self.current_promotion],
-            self.customers[self.current_customer]
-        )
-        self.current_customer += 1
-
-        return item_to_return
     
     def __iter__(self):
         return self
+    
+    def __getitem__(self, item):
+        all_combinations = len(self.products) * len(self.promototions) * len(self.customers)
+        if  all_combinations < item:
+            raise StopIteration("Iteration had stopped...")
+        else:
+            position_products = item // (len(self.promototions) * len(self.customers))
+            item = item % (len(self.promototions) * len(self.customers))
+            
+            position_promotions = item // len(self.customers)
+            item = item % len(self.customers)
+
+            position_cutomers = item
+
+        return "{} - {} - {}".format(
+            self.products[position_products],
+            self.promototions[position_promotions],
+            self.customers[position_cutomers]
+        ) 
--- Chapter_08/lab_39_40/main.py
+++ Chapter_08/lab_39_40/main.py
@@ -37,6 +37,18 @@ def test_2():
     pprint("test 2 - memory uage: {}".format(sys.getsizeof(combinations)))
 
 
+def item_example():
+    products = ["Products: {}".format(i) for i in range(1, 4)]
+    promototions = ["Promotions: {}".format(i) for i in range(1, 3)]
+    customers = ["Customers: {}".format(i) for i in range(1, 6)]
+    
+    combinations = Combinations(products, promototions, customers)
+
+    for i in range(1, 30):
+        print(i, combinations[i])
+
+
 if __name__ == "__main__":
-    test_1()
-    test_2()
+    # test_1()
+    # test_2()
+    item_example()
